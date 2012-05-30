1f93823388d3ae9b92f22a059ba1d9e1529ecc5b
raiden <raiden.ht@gmail.com>
2012-05-30 17:54:44 +0800

    numlist: script for module nod added

---
+++ numlist/module_load.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+module="numlist"
+device="numlist"
+mode="777"
+
+INSMOD=/sbin/insmod
+RMMOD=/sbin/rmmod
+
+installed=`lsmod | grep $module`
+if [ ! -z "$installed" ]
+then
+	$RMMOD $module
+fi
+
+$INSMOD $module.ko $* || exit 1
+
+rm -f /dev/${device}
+
+major=$(awk "\$2==\"$module\" {print \$1}" /proc/devices)
+if [ -z $major ]
+then
+	major=1234
+	echo cannot find device major, use default instead
+	#exit 1
+fi
+
+group="staff"
+grep -q '^staff:' /etc/group || group="wheel"
+
+type="c"
+
+mknod /dev/${device} $type $major 1
+chgrp $group /dev/${device}
+chmod $mode /dev/${device}
