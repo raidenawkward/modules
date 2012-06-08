73fa422d50bd1c436fe03f9dcae521ad2dfd807c
raiden <raiden.ht@gmail.com>
2012-06-08 14:39:16 +0800

    testworkqueue: sem used, work updated

--- workqueue/testworkqueue.c
+++ workqueue/testworkqueue.c
@@ -3,7 +3,9 @@
 #include <linux/workqueue.h>
 #include <linux/delay.h>
 #include <linux/string.h>
+#include <linux/semaphore.h>
 
+struct semaphore sem;
 
 struct struct_with_work {
 	struct work_struct work;
@@ -19,17 +21,27 @@ static void work_handler(struct work_struct *data)
 	int i;
 	struct struct_with_work *work= container_of(data, struct struct_with_work, work);
 
+	/* changes would be seen if sem disabled */
+	down(&sem);
+
 	for (i = 0; i < 10; ++i) {
 		printk(KERN_ALERT "work %s: %d", work->data, i);
-		mdelay(10);
+		// big difference between delay and sleep
+		//mdelay(5000);
+		msleep(1000);
 	}
+	up(&sem);
 }
 
 static int __init test_workqueue_init(void)
 {
 	int i;
 
-	queue = create_singlethread_workqueue("testworkqueue");
+	sema_init(&sem, 1);
+
+	printk(KERN_NOTICE "module testworkqueue initing");
+	//queue = create_singlethread_workqueue("testworkqueue");
+	queue = create_workqueue("testworkqueue");
 
 	if (!queue)
 		goto err;
@@ -39,7 +51,8 @@ static int __init test_workqueue_init(void)
 		sprintf(works[i].data, "%d", i);
 
 		INIT_WORK(&(works[i].work), work_handler);
-		queue_work(queue, &(works[i].work));
+		//queue_work(queue, &(works[i].work));
+		schedule_work(&(works[i].work));
 	}
 
 	return 0;
@@ -52,6 +65,7 @@ static void __exit test_workqueue_exit(void)
 {
 	if (queue)
 		destroy_workqueue(queue);
+	printk(KERN_NOTICE "module testworkqueue exited");
 }
 
 MODULE_LICENSE("GPL");
